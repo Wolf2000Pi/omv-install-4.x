3c9e231e8fe2de5e2e91c8511510097d055a09a3
Wolf2000 <wolf2000@aon.at>
2018-01-13 23:10:48 +0100

    DIV

--- omv-install-4.x.sh
+++ omv-install-4.x.sh
@@ -17,6 +17,8 @@ echo  Ihre Antwort war: $answer
 if [ "$answer" != "n" ]
   then echo "deb http://packages.openmediavault.org/public arrakis main" > /etc/apt/sources.list.d/openmediavault.list &&
 sleep 5
+apt install --force-yes curl wget apt-transport-https dirmngr
+sleep 5
 gpg --recv-keys 7E7A6C592EF35D13 &&
 sleep 5
 gpg --export 7E7A6C592EF35D13 |apt-key add - &&
@@ -43,7 +45,7 @@ apt-get --yes --force-yes --allow-unauthenticated install openmediavault &&
 sleep 1
 wget http://omv-extras.org/openmediavault-omvextrasorg_latest_all4.deb
 sleep 1
-dpkg -i openmediavault-omvextrasorg_4.0.1_all.deb
+dpkg -i openmediavault-omvextrasorg_latest_all4.deb
 sleep 1
 apt-get -f install
 sleep 1
